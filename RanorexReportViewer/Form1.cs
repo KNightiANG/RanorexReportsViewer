f6fd58271bccaebddec7f117dec048c94c8fbec3
Jeremy yang <knightiang@hotmail.com>
2018-01-17 19:46:47 +0800

    Initial commit

---
+++ RanorexReportViewer/Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RanorexReportViewer
+{
+    public partial class Form1 : Form
+    {
+        private string _defaultReportPath = @".\TestReprot\";
+        private List<string> _reports = new List<string>();
+        private int _depth = 0;
+        public Form1()
+        {
+            InitializeComponent();
+            loadAllReports(_defaultReportPath);
+        }
+        private void loadAllReports(string path)
+        {
+            IEnumerable<string> filesInFOlder = Directory.EnumerateFiles(path);
+            IEnumerable<string> directoriesInFolder = Directory.EnumerateDirectories(path);
+            if (filesInFOlder.Count<string>() > 0)
+            {
+                foreach (string s in filesInFOlder)
+                {
+                    if (s.ToLower().EndsWith("rxlog"))
+                    {
+                        _reports.Add(s);
+                    }
+                }
+            }
+
+            if (directoriesInFolder.Count<string>() > 0)
+            {
+                foreach (string d in directoriesInFolder)
+                {
+                    loadAllReports(d);
+                }
+            }
+        }
+    }
+}
